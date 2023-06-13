1fefd291bd19037997eb27581614a9a67f5ff9bd
Alec Hannaford <alec322@gmail.com>
2023-06-13 15:42:52 -0700

    added views

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Snack
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import SnackSerializer 
+
 
 # Create your views here.
+from .models import Snack 
+class TodoViewSets(viewsets.ModelViewSet):
+    queryset = Snack.objects.all()
+    serializer_class = SnackSerializer
+    permission_classes = [permissions.AllowAny]
